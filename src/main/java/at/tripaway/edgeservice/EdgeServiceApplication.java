0283fee5f45b56231f7e89389b884aa131ad8e38
Scaffolder <scaffolder@backstage.io>
2023-09-09 09:29:57 +0200

    initial commit

---
+++ src/main/java/at/tripaway/edgeservice/EdgeServiceApplication.java
@@ -0,0 +1,12 @@
+package at.tripaway.edgeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdgeServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EdgeServiceApplication.class, args);
+  }
+}
